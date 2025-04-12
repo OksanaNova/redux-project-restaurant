36f7c2426383db14d1967cb009ecb21eae89c996
OksanaNova <o.nova0804@gmail.com>
2025-04-12 12:05:41 -0600

    redux toolkit

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 const Cart = () => {
     return (
         <div>
-            <p>CART!!!</p>
+            <img className="cartIcon" src="https://img.icons8.com/external-xnimrodx-lineal-color-xnimrodx/64/000000/external-shopping-cart-cyber-monday-xnimrodx-lineal-color-xnimrodx.png " alt="cart-icon" />
         </div>
     )
 }
