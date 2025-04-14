37b678d3e09003ebafc33d7fbb1384ee6ac6a038
OksanaNova <o.nova0804@gmail.com>
2025-04-14 12:23:24 -0600

    filter category added

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -1,14 +1,17 @@
-import { useSelector } from "react-redux";
-import { getSelectedCategory } from "../../redux/dishesSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { filterCategory, getSelectedCategory } from "../../redux/dishesSlice";
 
 const Filter = ({category}) => {
+
+    const dispatch = useDispatch();
 
     const selectedCategory = useSelector(getSelectedCategory);
 
 
     return (
         <div className="category">
-            <p className={selectedCategory === category ? 'categoryButtonSelected categoryButton' : 'categoryButton' }>{category}</p>
+            <p onClick={() => {dispatch(filterCategory(category))}}
+             className={selectedCategory === category ? 'categoryButtonSelected categoryButton' : 'categoryButton' }>{category}</p>
         </div>
     )
 }
