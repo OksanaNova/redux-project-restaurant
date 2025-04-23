fadc13ecb97d12c4c5f3484597f10aea2aab59eb
OksanaNova <o.nova0804@gmail.com>
2025-04-22 19:54:58 -0600

    remove items button fixed

--- src/Components/Cart/CartItem.jsx
+++ src/Components/Cart/CartItem.jsx
@@ -1,16 +1,22 @@
 import dataDishes from "../../data/dataDishes"
+import { useDispatch } from "react-redux";
+import { removeItemFromCart } from "../../redux/cartSlice";
 
 const CartItem = ({cartElement}) => {
     console.log(cartElement)
 
-    const filteredDishes = dataDishes.find(item => item.id === cartElement.dishId)
-    console.log(filteredDishes)
+    const filteredDishes = dataDishes.find(item => item.id === cartElement.dishId);
+    const dispatch = useDispatch();
+    // console.log(filteredDishes)
 
     return (
         <div>
             <p>{filteredDishes.name} - <span>{cartElement.quantity} portion(s)</span></p>
             <p>Price: ${filteredDishes.price * cartElement.quantity}</p>
-            <img className="icon" src="https://img.icons8.com/material-outlined/48/000000/trash--v1.png" alt="trash-icon" />
+            <span onClick={() => dispatch(removeItemFromCart({cartElementId: cartElement.id}))}>
+                <img className="icon" src="https://img.icons8.com/material-outlined/48/000000/trash--v1.png" alt="trash-icon" />
+            </span>
+            
         </div>
     )
 }
